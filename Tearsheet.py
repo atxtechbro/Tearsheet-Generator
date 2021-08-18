dcdd133d78c766ca782b037ee787dce34dbe2de4
Morgan Joyce <84004324+mojo-py@users.noreply.github.com>
2021-08-17 19:22:34 -0500

    Update Tearsheet.py

--- Tearsheet.py
+++ Tearsheet.py
@@ -2,7 +2,8 @@ import requests
 import json
 import pandas as pd
 from pandas import json_normalize
-pd.options.display.max_rows = 100
+pd.options.display.max_rows = 20
+pd.options.display.max_columns = 12
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
